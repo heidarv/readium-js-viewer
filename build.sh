b474ae55b3b77e748acad42071790b1dea73dea1
Heidar Vakilzadeh <heidarv@gmail.com>
2018-01-17 20:46:19 +0330

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+git submodule update --init --recursive
+git checkout BRANCH_NAME && git submodule foreach --recursive "git checkout BRANCH_NAME"
+npm run prepare:all
+npm install
+npm update
+npm run build
